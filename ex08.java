abce44a2c88876af6e34a0c8ac9f7c825be385ab
Park Hyeon Beom <gusqja0213@naver.com>
2023-11-21 17:44:32 +0900

    [Test] 문제풀이

--- ex08.java
+++ ex08.java
@@ -1,26 +1,20 @@
-import java.util.*;
 public class ex08 {
     public static void main(String[] args) {
-
-        int num[] = new int[6];
-
-        for (int i = 0; i < 6; i++) {
-            num[i] = (int) (Math.random() * 6) + 1;
-        }
-
-        for (int i = 0; i < 6; i++) {
-            for (int j = i+1; j <6; j++) {
-                if (num[i] == num[j])
-                    while(true)
-                    {
-                        num[i] = (int) (Math.random() * 6) + 1;
-                        if (num[i] != num[j])
-                            break;
-                    }
+        int lotto[] = new int[6];	// 로또 번호를 입력받을 배열을 선언해준다.
+        System.out.print("로또 번호 : ");
+        for(int i = 0; i < lotto.length; i++)
+        {
+            lotto[i] = (int)(Math.random() * 45) + 1; // 1~45까지의 임의의 수를 받는다.
+            for(int j = 0; j < i; j++)
+            {	// 중복된 번호가 있으면 이전 포문으로 돌아가 다시 시행한다.
+                if(lotto[i] == lotto[j])
+                {
+                    i--;
+                }
             }
+            System.out.print(lotto[i] + " ");	// 로또번호를 출력한다.
         }
-        for (int i = 0; i < 6; i++) {
-            System.out.println(num[i]);
-        }
+
     }
 }
+		
